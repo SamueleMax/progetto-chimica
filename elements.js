60586900f6d712eaae18a3dec605e9e79441805b
SamueleMax <max.samuele@itiscannizzaro.edu.it>
2025-04-10 09:50:18 +0200

    Working on stuff

---
+++ elements.js
@@ -0,0 +1,44 @@
+const elements = [
+    {
+        name: 'Al',
+        element: 'Al',
+        type: 'cation',
+        charge: 3
+    },
+    {
+        name: 'Ba',
+        element: 'Ba',
+        type: 'cation',
+        charge: 2
+    },
+    {
+        name: 'Na',
+        element: 'Na',
+        type: 'cation',
+        charge: 1
+    },
+    {
+        name: 'Cl',
+        element: 'Cl',
+        type: 'anion',
+        charge: -1
+    },
+    {
+        name: 'SO',
+        element: 'SO4',
+        type: 'anion',
+        charge: -2
+    },
+    {
+        name: 'CO',
+        element: 'CO3',
+        type: 'anion',
+        charge: -2
+    },
+    {
+        name: 'PO',
+        element: 'PO4',
+        type: 'anion',
+        charge: -3
+    }
+];
--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let canvasElements = [];
+// Store the cation and anion name and count
+let cation = { name : '', count : 0 };
+let anion = { name: '', count: 0 };
 
 function resizeCanvas() {
     const main = document.querySelector('main');
@@ -42,9 +44,28 @@ function initCanvas() {
 
         canvas.classList.remove('canvas-dragenter');
 
-        // This contains the element name
-        const data = e.dataTransfer.getData('text/plain');
-        canvasElements.push(data);
+        // The element name
+        const element = e.dataTransfer.getData('text/plain');
+        const elementData = elements.find(e => e.name === element);
+
+        if (elementData.type === 'cation') {
+            // Check if a cation has already been selected
+            if (cation.name !== '' && cation.name !== element) {
+                alert('Per cambiare catione di partenza, resetta la pagina.');
+                return;
+            }
+            cation.name = element;
+            cation.count += 1;
+        } else {
+            // Check if an anion has already been selected
+            if (anion.name !== '' && anion.name !== element) {
+                alert('Per cambiare anione di partenza, resetta la pagina.');
+                return;
+            }
+            anion.name = element;
+            anion.count += 1;
+        }
+        
         renderCanvas();
     });
 }
@@ -54,17 +75,63 @@ function renderCanvas() {
     const context = canvas.getContext('2d');
     context.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
 
-    canvasElements.forEach(element => {
+    const imgScale = 0.1
+
+    // Render cations
+    if (cation.name) {
         const img = new Image();
-        img.src = `assets/${element}.png`;
+        img.src = `assets/${cation.name}.png`;
+
+        const imgPos = calcElementPos('cation', imgScale, canvas);
+
         img.onload = () => {
-            context.drawImage(img, 0, 0, 300, 300);
+            context.drawImage(img, imgPos.x, imgPos.y, img.naturalWidth * imgScale, img.naturalHeight * imgScale);
         };
-    });
+    }
+
+    // Render anions
+    if (anion.name) {
+        const img = new Image();
+        img.src = `assets/${anion.name}.png`;
+
+        const imgPos = calcElementPos('anion', imgScale, canvas);
+
+        img.onload = () => {
+            context.drawImage(img, imgPos.x, imgPos.y, img.naturalWidth * imgScale, img.naturalHeight * imgScale);
+        };
+    }
+}
+
+function calcElementPos(type, imgScale, canvas) {
+    const cationWidth = 1771 * imgScale;
+    const anionWidth = 1980 * imgScale;
+    const baseHeight = 625 * imgScale; // Base image height (*2 if +2, *3 if +3, etc.)
+
+    let posX = 0;
+    let posY = 0;
+
+    if (type === 'cation') {
+        let elementData = elements.find(e => e.name === cation.name);
+
+        // Calculate x pos, putting the img in the left half of the canvas
+        posX = canvas.width / 2 - cationWidth + 11;
+
+        // Calculate y pos
+        posY = canvas.height / 2 - (baseHeight / 2 * Math.abs(elementData.charge));
+    } else {
+        let elementData = elements.find(e => e.name === anion.name);
+
+        posX = canvas.width / 2 - 11;
+
+        posY = canvas.height / 2 - (baseHeight / 2 * Math.abs(elementData.charge));
+    }
+
+    return { x: posX, y: posY };
 }
 
 function resetCanvas() {
-    canvasElements = [];
+    cation = { name: '', count: 0 };
+    anion = { name: '', count: 0 };
     renderCanvas();
 }
 
